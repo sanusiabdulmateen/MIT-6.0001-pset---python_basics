2538a58a3c395dc2915ea54068cb49ef2686e33e
Sanusi Abdulmateen <sanusiabdulmateen@gmail.com>
2023-08-19 16:02:44 +0100

    First commit

---
+++ ps0/ps0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 14:37:29 2023
+
+@author: sanus
+"""
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y =", x**y)
+log_ans = numpy.log2(x)
+print("log(x) =", int(log_ans))
